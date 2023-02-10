ac55363ccfed5c1607f06a3a9de66408ac5ef797
SDW <stephenoilesanmi@gmail.com>
2023-02-10 22:42:04 +0000

    created script for raw_sales_agents_tbl

--- dwh_pipelines/1_raw_layer/raw_sales_agents_tbl.py
+++ dwh_pipelines/1_raw_layer/raw_sales_agents_tbl.py
@@ -58,7 +58,7 @@ root_logger.addHandler(console_handler)
 USING_AIRFLOW   =   False
 
 # Create source file variable 
-src_file    =   'flight_promotion_deals.json'
+src_file    =   'sales_agents.json'
 
 
 # Create a config file for storing environment variables
@@ -67,7 +67,7 @@ if USING_AIRFLOW:
 
     # Use the airflow config file from the airflow container 
     config.read('/usr/local/airflow/dags/etl_to_postgres/airflow_config.ini')
-    flight_promotion_deals_path = config['postgres_airflow_config']['DATASET_SOURCE_PATH'] + src_file
+    sales_agents_path = config['postgres_airflow_config']['DATASET_SOURCE_PATH'] + src_file
 
     host                    =   config['postgres_airflow_config']['HOST']
     port                    =   config['postgres_airflow_config']['PORT']
@@ -84,7 +84,7 @@ else:
     # Use the local config file from the local machine 
     path    =   os.path.abspath('dwh_pipelines/local_config.ini')
     config.read(path)
-    flight_promotion_deals_path     =   config['travel_data_filepath']['DATASETS_LOCATION_PATH'] + src_file
+    sales_agents_path     =   config['travel_data_filepath']['DATASETS_LOCATION_PATH'] + src_file
 
     host                    =   config['travel_data_filepath']['HOST']
     port                    =   config['travel_data_filepath']['PORT']
@@ -104,18 +104,18 @@ root_logger.info("Beginning the source data extraction process...")
 COMPUTE_START_TIME  =   time.time()
 
 
-with open(flight_promotion_deals_path, 'r') as flight_promotion_deals_file:    
+with open(sales_agents_path, 'r') as sales_agents_file:    
     
     try:
-        flight_promotion_deals_data = json.load(flight_promotion_deals_file)
-        flight_promotion_deals_data = flight_promotion_deals_data[0:100]
+        sales_agents_data = json.load(sales_agents_file)
+        sales_agents_data = sales_agents_data[0:100]
         root_logger.info(f"Successfully located '{src_file}'")
-        root_logger.info(f"File type: '{type(flight_promotion_deals_data)}'")
+        root_logger.info(f"File type: '{type(sales_agents_data)}'")
 
     except:
         root_logger.error("Unable to locate source file...terminating process...")
         raise Exception("No source file located")
-    
+
 
 postgres_connection = psycopg2.connect(
                 host        =   host,
@@ -133,7 +133,7 @@ def load_data_to_raw_layer(postgres_connection):
         CURRENT_TIMESTAMP               =   datetime.now()
         db_layer_name                   =   database
         schema_name                     =   'main'
-        table_name                      =   'raw_flight_promotion_deals_tbl'
+        table_name                      =   'raw_sales_agents_tbl'
         data_warehouse_layer            =   'RAW'
         source_system                   =   ['CRM', 'ERP', 'Mobile App', 'Website', '3rd party apps', 'Company database']
         row_counter                     =   0 
@@ -176,25 +176,32 @@ def load_data_to_raw_layer(postgres_connection):
 
 
         # Set up SQL statements for table deletion and validation check  
-        delete_raw_flight_promotion_deals_tbl_if_exists     =   f''' DROP TABLE IF EXISTS {schema_name}.{table_name} CASCADE;
+        delete_raw_sales_agents_tbl_if_exists     =   f''' DROP TABLE IF EXISTS {schema_name}.{table_name} CASCADE;
         '''
 
-        check_if_raw_flight_promotion_deals_tbl_is_deleted  =   f'''   SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}' );
+        check_if_raw_sales_agents_tbl_is_deleted  =   f'''   SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}' );
         '''
 
         # Set up SQL statements for table creation and validation check 
-        create_raw_flight_promotion_deals_tbl = f'''                CREATE TABLE IF NOT EXISTS {schema_name}.{table_name} (
-                                                                                promotion_id            VARCHAR PRIMARY KEY NOT NULL,
-                                                                                flight_booking_id       UUID NOT NULL,
-                                                                                promotion_name          VARCHAR(255) NOT NULL,
-                                                                                applied_discount        NUMERIC(18, 2) NOT NULL
+        create_raw_sales_agents_tbl = f'''                CREATE TABLE IF NOT EXISTS {schema_name}.{table_name} (
+                                                                            id                              UUID PRIMARY KEY,
+                                                                            first_name                      VARCHAR(255) NOT NULL,
+                                                                            last_name                       VARCHAR(255) NOT NULL,
+                                                                            email                           VARCHAR(255) NOT NULL,
+                                                                            phone                           VARCHAR(255) NOT NULL,
+                                                                            location                        VARCHAR(255) NOT NULL,
+                                                                            nationality                     VARCHAR(255) NOT NULL,
+                                                                            seniority_level                 VARCHAR(255) NOT NULL,
+                                                                            service_speciality              VARCHAR(255) NOT NULL,
+                                                                            commission                      NUMERIC(18, 6) NOT NULL,
+                                                                            years_experience                INTEGER NOT NULL
                                                                         );
 
 
 
         '''
 
-        check_if_raw_flight_promotion_deals_tbl_exists  =   f'''       SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}' );
+        check_if_raw_sales_agents_tbl_exists  =   f'''       SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}' );
         '''
 
        
@@ -202,7 +209,7 @@ def load_data_to_raw_layer(postgres_connection):
 
 
         # Set up SQL statements for adding data lineage and validation check 
-        add_data_lineage_to_raw_flight_promotion_deals_tbl  =   f'''        ALTER TABLE {schema_name}.{table_name}
+        add_data_lineage_to_raw_sales_agents_tbl  =   f'''        ALTER TABLE {schema_name}.{table_name}
                                                                                 ADD COLUMN  created_at                  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                                                                                 ADD COLUMN  updated_at                  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                                                                                 ADD COLUMN  source_system               VARCHAR(255),
@@ -229,11 +236,18 @@ def load_data_to_raw_layer(postgres_connection):
         '''
 
         # Set up SQL statements for records insert and validation check
-        insert_flight_promotion_deals_data  =   f'''                       INSERT INTO {schema_name}.{table_name} (
-                                                                                flight_booking_id, 
-                                                                                promotion_id, 
-                                                                                promotion_name, 
-                                                                                applied_discount,
+        insert_sales_agents_data  =   f'''                       INSERT INTO {schema_name}.{table_name} (
+                                                                                id,
+                                                                                first_name,
+                                                                                last_name,
+                                                                                email,
+                                                                                phone,
+                                                                                location,
+                                                                                nationality,
+                                                                                seniority_level,
+                                                                                service_speciality,
+                                                                                commission,
+                                                                                years_experience,
                                                                                 created_at,
                                                                                 updated_at,
                                                                                 source_system,
@@ -242,7 +256,7 @@ def load_data_to_raw_layer(postgres_connection):
                                                                                 dwh_layer
                                                                             )
                                                                             VALUES (
-                                                                                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                                                                                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
                                                                             );
         '''
 
@@ -304,12 +318,12 @@ def load_data_to_raw_layer(postgres_connection):
 
         # Delete table if it exists in Postgres
         DELETING_SCHEMA_PROCESSING_START_TIME   =   time.time()
-        cursor.execute(delete_raw_flight_promotion_deals_tbl_if_exists)
+        cursor.execute(delete_raw_sales_agents_tbl_if_exists)
         DELETING_SCHEMA_PROCESSING_END_TIME     =   time.time()
 
         
         DELETING_SCHEMA_VAL_CHECK_PROCESSING_START_TIME     =   time.time()
-        cursor.execute(check_if_raw_flight_promotion_deals_tbl_is_deleted)
+        cursor.execute(check_if_raw_sales_agents_tbl_is_deleted)
         DELETING_SCHEMA_VAL_CHECK_PROCESSING_END_TIME       =   time.time()
 
 
@@ -318,14 +332,14 @@ def load_data_to_raw_layer(postgres_connection):
             root_logger.debug(f"")
             root_logger.info(f"=============================================================================================================================================================================")
             root_logger.info(f"TABLE DELETION SUCCESS: Managed to drop {table_name} table in {db_layer_name}. Now advancing to recreating table... ")
-            root_logger.info(f"SQL Query for validation check:  {check_if_raw_flight_promotion_deals_tbl_is_deleted} ")
+            root_logger.info(f"SQL Query for validation check:  {check_if_raw_sales_agents_tbl_is_deleted} ")
             root_logger.info(f"=============================================================================================================================================================================")
             root_logger.debug(f"")
         else:
             root_logger.debug(f"")
             root_logger.error(f"==========================================================================================================================================================================")
             root_logger.error(f"TABLE DELETION FAILURE: Unable to delete {table_name}. This table may have objects that depend on it (use DROP TABLE ... CASCADE to resolve) or it doesn't exist. ")
-            root_logger.error(f"SQL Query for validation check:  {check_if_raw_flight_promotion_deals_tbl_is_deleted} ")
+            root_logger.error(f"SQL Query for validation check:  {check_if_raw_sales_agents_tbl_is_deleted} ")
             root_logger.error(f"==========================================================================================================================================================================")
             root_logger.debug(f"")
 
@@ -333,12 +347,12 @@ def load_data_to_raw_layer(postgres_connection):
 
         # Create table if it doesn't exist in Postgres  
         CREATING_TABLE_PROCESSING_START_TIME    =   time.time()
-        cursor.execute(create_raw_flight_promotion_deals_tbl)
+        cursor.execute(create_raw_sales_agents_tbl)
         CREATING_TABLE_PROCESSING_END_TIME  =   time.time()
 
         
         CREATING_TABLE_VAL_CHECK_PROCESSING_START_TIME  =   time.time()
-        cursor.execute(check_if_raw_flight_promotion_deals_tbl_exists)
+        cursor.execute(check_if_raw_sales_agents_tbl_exists)
         CREATING_TABLE_VAL_CHECK_PROCESSING_END_TIME    =   time.time()
 
 
@@ -347,14 +361,14 @@ def load_data_to_raw_layer(postgres_connection):
             root_logger.debug(f"")
             root_logger.info(f"=============================================================================================================================================================================")
             root_logger.info(f"TABLE CREATION SUCCESS: Managed to create {table_name} table in {db_layer_name}.  ")
-            root_logger.info(f"SQL Query for validation check:  {check_if_raw_flight_promotion_deals_tbl_exists} ")
+            root_logger.info(f"SQL Query for validation check:  {check_if_raw_sales_agents_tbl_exists} ")
             root_logger.info(f"=============================================================================================================================================================================")
             root_logger.debug(f"")
         else:
             root_logger.debug(f"")
             root_logger.error(f"==========================================================================================================================================================================")
             root_logger.error(f"TABLE CREATION FAILURE: Unable to create {table_name}... ")
-            root_logger.error(f"SQL Query for validation check:  {check_if_raw_flight_promotion_deals_tbl_exists} ")
+            root_logger.error(f"SQL Query for validation check:  {check_if_raw_sales_agents_tbl_exists} ")
             root_logger.error(f"==========================================================================================================================================================================")
             root_logger.debug(f"")
 
@@ -362,7 +376,7 @@ def load_data_to_raw_layer(postgres_connection):
 
         # Add data lineage to table 
         ADDING_DATA_LINEAGE_PROCESSING_START_TIME   =   time.time()
-        cursor.execute(add_data_lineage_to_raw_flight_promotion_deals_tbl)
+        cursor.execute(add_data_lineage_to_raw_sales_agents_tbl)
         ADDING_DATA_LINEAGE_PROCESSING_END_TIME     =   time.time()
 
         
@@ -397,28 +411,29 @@ def load_data_to_raw_layer(postgres_connection):
         root_logger.info(f"Rows before SQL insert in Postgres: {sql_result} ")
         root_logger.debug(f"")
 
-        used_ids = []
 
-        for flight_promotion_deals in flight_promotion_deals_data:
-            flight_promotion_deal_id = flight_promotion_deals['flight_booking_id']
-            if flight_promotion_deal_id in used_ids:
-                continue
-            else:
-                used_ids.append(flight_promotion_deal_id)
-                values = (
-                    flight_promotion_deals['flight_booking_id'], 
-                    flight_promotion_deals['promotion_id'], 
-                    flight_promotion_deals['promotion_name'], 
-                    flight_promotion_deals['applied_discount'],
-                    CURRENT_TIMESTAMP,
-                    CURRENT_TIMESTAMP,
-                    random.choice(source_system),
-                    src_file,
-                    CURRENT_TIMESTAMP,
-                    'RAW'
-                    )
+        for sales_agents in sales_agents_data:
+            values = (
+                sales_agents['id'],
+                sales_agents['first_name'],
+                sales_agents['last_name'],
+                sales_agents['email'],
+                sales_agents['phone'],
+                sales_agents['location'],
+                sales_agents['nationality'],
+                sales_agents['seniority_level'],
+                sales_agents['service_speciality'],
+                sales_agents['commission'],
+                sales_agents['years_experience'],
+                CURRENT_TIMESTAMP,
+                CURRENT_TIMESTAMP,
+                random.choice(source_system),
+                src_file,
+                CURRENT_TIMESTAMP,
+                'RAW'
+                )
 
-                cursor.execute(insert_flight_promotion_deals_data, values)
+            cursor.execute(insert_sales_agents_data, values)
 
 
             # Validate if each row inserted into the table exists 
@@ -426,13 +441,13 @@ def load_data_to_raw_layer(postgres_connection):
                 row_counter += 1
                 successful_rows_upload_count += 1
                 root_logger.debug(f'---------------------------------')
-                root_logger.info(f'INSERT SUCCESS: Uploaded flight_promotion_deals record no {row_counter} ')
+                root_logger.info(f'INSERT SUCCESS: Uploaded sales_agents record no {row_counter} ')
                 root_logger.debug(f'---------------------------------')
             else:
                 row_counter += 1
                 failed_rows_upload_count +=1
                 root_logger.error(f'---------------------------------')
-                root_logger.error(f'INSERT FAILED: Unable to insert flight_promotion_deals record no {row_counter} ')
+                root_logger.error(f'INSERT FAILED: Unable to insert sales_agents record no {row_counter} ')
                 root_logger.error(f'---------------------------------')
 
 
@@ -475,7 +490,17 @@ def load_data_to_raw_layer(postgres_connection):
         
 
         # Add a flag for confirming if sensitive data fields have been highlighted  
-        sensitive_columns_selected = [None
+        sensitive_columns_selected = ['id',
+                                    'first_name',
+                                    'last_name',
+                                    'email',
+                                    'phone',
+                                    'location',
+                                    'nationality',
+                                    'seniority_level',
+                                    'service_speciality',
+                                    'commission',
+                                    'years_experience'
                             ]
         
         
--- dwh_pipelines/task_checklist.md
+++ dwh_pipelines/task_checklist.md
@@ -16,7 +16,7 @@ This is a recollection of all the micro tasks for each DWH layer
 | DWH-005 | RAW             | Load source data for “Raw customer demographic data” into raw table `raw_customer_demographic_tbl`| SRC_TO_RAW | NOT_STARTED |
 | DWH-006 | RAW             | Load source data for “Flight destination information” into raw table `raw_flight_destination_tbl`| SRC_TO_RAW | NOT_STARTED |
 | DWH-007 | RAW             | Load source data for “Flight ticket sales” into raw table `raw_flight_ticket_sales_tbl`| SRC_TO_RAW | COMPLETED |
-| DWH-008 | RAW             | Load source data for “Flight Promotion” into raw table `raw_flight_promotion_tbl`| SRC_TO_RAW | NOT_STARTED |
+| DWH-008 | RAW             | Load source data for “Flight Promotion” into raw table `raw_flight_promotion_tbl`| SRC_TO_RAW | COMPLETED |
 | DWH-009 | RAW             | Load source data for “Holiday data” into raw table `raw_holiday_tbl`| SRC_TO_RAW | NOT_STARTED |
 | DWH-010 | RAW             | Load source data for “Airline data” into raw table `raw_airline_tbl`| SRC_TO_RAW | NOT_STARTED |
 | DWH-011 | RAW             | Load source data for “Sales agent data” into raw table `raw_sales_agent_tbl`| SRC_TO_RAW | NOT_STARTED |
@@ -30,7 +30,7 @@ This is a recollection of all the micro tasks for each DWH layer
 | DWH-019 | RAW             | Mark the sensitive fields (PII/PHI) in `raw_customer_demographic_tbl`| SENSITIVE_DATA_MARKING | NOT_STARTED |
 | DWH-020 | RAW             | Mark the sensitive fields (PII/PHI) in `raw_flight_destination_tbl`| SENSITIVE_DATA_MARKING | NOT_STARTED |
 | DWH-021 | RAW             | Mark the sensitive fields (PII/PHI) in `raw_flight_ticket_sales_tbl`| SENSITIVE_DATA_MARKING | COMPLETED |
-| DWH-022 | RAW             | Mark the sensitive fields (PII/PHI) in `raw_flight_promotion_tbl`| SENSITIVE_DATA_MARKING | NOT_STARTED |
+| DWH-022 | RAW             | Mark the sensitive fields (PII/PHI) in `raw_flight_promotion_tbl`| SENSITIVE_DATA_MARKING | COMPLETED |
 | DWH-023 | RAW             | Mark the sensitive fields (PII/PHI) in `raw_airline_tbl`| SENSITIVE_DATA_MARKING | NOT_STARTED |
 | DWH-024 | RAW             | Mark the sensitive fields (PII/PHI) in `raw_sales_agent_tbl`| SENSITIVE_DATA_MARKING | NOT_STARTED |
 | DWH-025 | RAW             | Mark the sensitive fields (PII/PHI) in `raw_ticket_sales_tbl`| SENSITIVE_DATA_MARKING | NOT_STARTED |
@@ -43,7 +43,7 @@ This is a recollection of all the micro tasks for each DWH layer
 | DWH-032 | RAW             | Add event logs to `raw_customer_demographic_tbl`|    LOGGING_RAW_LEVEL_EVENTS  | NOT_STARTED |
 | DWH-033 | RAW             | Add event logs to `raw_flight_destination_tbl`|    LOGGING_RAW_LEVEL_EVENTS  | NOT_STARTED |
 | DWH-034 | RAW             | Add event logs to `raw_flight_ticket_sales_tbl`|    LOGGING_RAW_LEVEL_EVENTS  | COMPLETED |
-| DWH-035 | RAW             | Add event logs to `raw_flight_promotion_tbl`|    LOGGING_RAW_LEVEL_EVENTS  | NOT_STARTED |
+| DWH-035 | RAW             | Add event logs to `raw_flight_promotion_tbl`|    LOGGING_RAW_LEVEL_EVENTS  | COMPLETED |
 | DWH-036 | RAW             | Add event logs to `raw_airline_tbl`|    LOGGING_RAW_LEVEL_EVENTS  | NOT_STARTED |
 | DWH-037 | RAW             | Add event logs to `raw_sales_agent_tbl`|    LOGGING_RAW_LEVEL_EVENTS  | NOT_STARTED |
 | DWH-038 | RAW             | Add event logs to `raw_ticket_sales_tbl`|    LOGGING_RAW_LEVEL_EVENTS  | NOT_STARTED |
@@ -56,7 +56,7 @@ This is a recollection of all the micro tasks for each DWH layer
 | DWH-045 | RAW             | Design and run data profiling checks on `raw_customer_demographic_tbl`|    DATA_PROFILING_CHECKS  | NOT_STARTED |
 | DWH-046 | RAW             | Design and run data profiling checks on `raw_flight_destination_tbl`|    DATA_PROFILING_CHECKS  | NOT_STARTED |
 | DWH-047 | RAW             | Design and run data profiling checks on `raw_flight_ticket_sales_tbl`|    DATA_PROFILING_CHECKS  | COMPLETED |
-| DWH-048 | RAW             | Design and run data profiling checks on `raw_flight_promotion_tbl`|    DATA_PROFILING_CHECKS  | NOT_STARTED |
+| DWH-048 | RAW             | Design and run data profiling checks on `raw_flight_promotion_tbl`|    DATA_PROFILING_CHECKS  | COMPLETED |
 | DWH-049 | RAW             | Design and run data profiling checks on `raw_airline_tbl`|    DATA_PROFILING_CHECKS  | NOT_STARTED |
 | DWH-050 | RAW             | Design and run data profiling checks on `raw_sales_agent_tbl`|    DATA_PROFILING_CHECKS  | NOT_STARTED |
 | DWH-051 | RAW             | Design and run data profiling checks on `raw_ticket_sales_tbl`|    DATA_PROFILING_CHECKS  | NOT_STARTED |
