1e886770ef55f4293e161ae8195ae84154516ee2
SDW <stephenoilesanmi@gmail.com>
2023-02-11 22:08:08 +0000

    added more logs to orchestration script

--- dwh_pipelines/orchestration/dwh_flows.py
+++ dwh_pipelines/orchestration/dwh_flows.py
@@ -59,7 +59,7 @@ def generate_synthetic_travel_data():
     module_name = 'dwh_pipelines.L0_src_data_generator.src_data_generator'
     imported_function = 'generate_travel_data'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -75,7 +75,7 @@ def load_data_to_raw_accommodation_bookings_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_accommodation_bookings_tbl'
     imported_function = 'load_accommodation_bookings_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -86,7 +86,7 @@ def load_data_to_raw_customer_feedbacks_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_customer_feedbacks_tbl'
     imported_function = 'load_customer_feedbacks_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -97,7 +97,7 @@ def load_data_to_raw_customer_info_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_customer_info_tbl'
     imported_function = 'load_customer_info_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -108,7 +108,7 @@ def load_data_to_raw_flight_bookings_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_bookings_tbl'
     imported_function = 'load_flight_bookings_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -119,7 +119,7 @@ def load_data_to_raw_flight_destinations_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_destinations_tbl'
     imported_function = 'load_flight_destinations_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -130,7 +130,7 @@ def load_data_to_raw_flight_promotion_deals_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_promotion_deals_tbl'
     imported_function = 'load_flight_promotion_deals_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -141,7 +141,7 @@ def load_data_to_raw_flight_schedules_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_schedules_tbl'
     imported_function = 'load_flight_schedules_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -152,7 +152,7 @@ def load_data_to_raw_flight_ticket_sales_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_ticket_sales_tbl'
     imported_function = 'load_flight_ticket_sales_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -163,7 +163,7 @@ def load_data_to_raw_sales_agents_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_sales_agents_tbl'
     imported_function = 'load_sales_agents_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -174,7 +174,7 @@ def load_data_to_raw_ticket_prices_tbl():
     module_name = 'dwh_pipelines.L1_raw_layer.raw_ticket_prices_tbl'
     imported_function = 'load_ticket_prices_data_to_raw_table'
     root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from  '{module_name}' module")
+    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
     root_logger.info("")
 
 
@@ -191,35 +191,65 @@ def generate_source_data_flow():
 # Set up sub-flow for executing tasks in raw layer 
 @flow(name="Execute tasks in raw layer", flow_run_name="source_to_raw_layer_flow")
 def run_raw_layer_flow():
+
     load_data_to_raw_accommodation_bookings_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_accommodation_bookings_tbl'! ")
+
+
+
     load_data_to_raw_customer_feedbacks_tbl()
-    load_data_to_raw_customer_feedbacks_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_customer_feedbacks_tbl'! ")
+
+
+
     load_data_to_raw_customer_info_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_customer_info_tbl'! ")
+
+
+
     load_data_to_raw_flight_bookings_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_bookings_tbl'! ")
+
+
+
     load_data_to_raw_flight_destinations_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_destinations_tbl'! ")
+
+
+
     load_data_to_raw_flight_promotion_deals_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_promotion_deals_tbl'! ")
+
+
+
     load_data_to_raw_flight_schedules_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_schedules_tbl'! ")
+
+
+
     load_data_to_raw_flight_ticket_sales_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_ticket_sales_tbl'! ")
+
+
+
     load_data_to_raw_sales_agents_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_sales_agents_tbl'! ")
+
+
+
     load_data_to_raw_ticket_prices_tbl()
+    root_logger.info("SUCCESS! Completed loading source data into 'raw_ticket_prices_tbl'! ")
 
-
-
-
-# # Set up sub-flow dependencies 
-# @flow(name="Run data warehouse tasks")
-# def run_main_dwh_flow():
-#     L0_GENERATE_DATA_FLOW   =   generate_source_data_flow()
-#     L1_RAW_LAYER_FLOW       =   run_raw_layer_flow()
-
-
-#     # L1_RAW_LAYER_FLOW.set_upstream(L0_GENERATE_DATA_FLOW)
-#     L0_GENERATE_DATA_FLOW.set_upstream(L1_RAW_LAYER_FLOW)
     
-# generate_source_data_flow().run(run_name="generate_data")
+    root_logger.info("Now terminating session for raw layer tasks...")
+    root_logger.info("")
+    root_logger.info("Raw tables processing session ended.")
+
+
+
+
+
+# Specify flow execution order in DAG-less manner  
 
 generate_source_data_flow()
 run_raw_layer_flow()
-
-
-# run_main_dwh_flow()
