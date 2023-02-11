dfce5fdb64d8268f8f99a7f312c73be360ef8d1f
SDW <stephenoilesanmi@gmail.com>
2023-02-11 22:30:54 +0000

    replaced custom loggers with get_run_logger

--- dwh_pipelines/orchestration/dwh_flows.py
+++ dwh_pipelines/orchestration/dwh_flows.py
@@ -1,57 +1,6 @@
-from prefect import task, flow
+from prefect import task, flow, get_run_logger
 from prefect.deployments import Deployment
 from prefect.orion.schemas.schedules import IntervalSchedule
-import logging, coloredlogs
-from pathlib import Path
-
-# ================================================ LOGGER ================================================
-
-# Set up root root_logger 
-root_logger     =   logging.getLogger(__name__)
-root_logger.setLevel(logging.DEBUG)
-
-
-# Set up formatter for logs 
-file_handler_log_formatter      =   logging.Formatter('%(asctime)s  |  %(levelname)s  |  %(message)s  ')
-console_handler_log_formatter   =   coloredlogs.ColoredFormatter(fmt    =   '%(message)s', level_styles=dict(
-                                                                                                debug           =   dict    (color  =   'white'),
-                                                                                                info            =   dict    (color  =   'green'),
-                                                                                                warning         =   dict    (color  =   'cyan'),
-                                                                                                error           =   dict    (color  =   'red',      bold    =   True,   bright      =   True),
-                                                                                                critical        =   dict    (color  =   'black',    bold    =   True,   background  =   'red')
-                                                                                            ),
-
-                                                                                    field_styles=dict(
-                                                                                        messages            =   dict    (color  =   'white')
-                                                                                    )
-                                                                                    )
-
-
-# Set up file handler object for logging events to file
-current_filepath    =   Path(__file__).stem
-file_handler        =   logging.FileHandler('logs/orchestration/' + current_filepath + '.log', mode='w')
-file_handler.setFormatter(file_handler_log_formatter)
-
-
-# Set up console handler object for writing event logs to console in real time (i.e. streams events to stderr)
-console_handler     =   logging.StreamHandler()
-console_handler.setFormatter(console_handler_log_formatter)
-
-
-# Add the file and console handlers 
-root_logger.addHandler(file_handler)
-
-
-# Only add the console handler if the script is running directly from this location 
-
-ACTIVATE_PREFECT_CONSOLE_STREAMS_VIA_CUSTOM_LOGGER = False
-
-if ACTIVATE_PREFECT_CONSOLE_STREAMS_VIA_CUSTOM_LOGGER:
-    root_logger.addHandler(console_handler)
-
-
-
-
 
 
 
@@ -63,9 +12,9 @@ def generate_synthetic_travel_data():
     from dwh_pipelines.L0_src_data_generator.src_data_generator import  generate_travel_data
     module_name = 'dwh_pipelines.L0_src_data_generator.src_data_generator'
     imported_function = 'generate_travel_data'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -79,9 +28,9 @@ def load_data_to_raw_accommodation_bookings_tbl():
     from dwh_pipelines.L1_raw_layer.raw_accommodation_bookings_tbl      import  load_accommodation_bookings_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_accommodation_bookings_tbl'
     imported_function = 'load_accommodation_bookings_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -90,9 +39,9 @@ def load_data_to_raw_customer_feedbacks_tbl():
     from dwh_pipelines.L1_raw_layer.raw_customer_feedbacks_tbl          import  load_customer_feedbacks_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_customer_feedbacks_tbl'
     imported_function = 'load_customer_feedbacks_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -101,9 +50,9 @@ def load_data_to_raw_customer_info_tbl():
     from dwh_pipelines.L1_raw_layer.raw_customer_info_tbl               import  load_customer_info_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_customer_info_tbl'
     imported_function = 'load_customer_info_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -112,9 +61,9 @@ def load_data_to_raw_flight_bookings_tbl():
     from dwh_pipelines.L1_raw_layer.raw_flight_bookings_tbl             import  load_flight_bookings_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_bookings_tbl'
     imported_function = 'load_flight_bookings_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -123,9 +72,9 @@ def load_data_to_raw_flight_destinations_tbl():
     from dwh_pipelines.L1_raw_layer.raw_flight_destinations_tbl         import  load_flight_destinations_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_destinations_tbl'
     imported_function = 'load_flight_destinations_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -134,9 +83,9 @@ def load_data_to_raw_flight_promotion_deals_tbl():
     from dwh_pipelines.L1_raw_layer.raw_flight_promotion_deals_tbl      import  load_flight_promotion_deals_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_promotion_deals_tbl'
     imported_function = 'load_flight_promotion_deals_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -145,9 +94,9 @@ def load_data_to_raw_flight_schedules_tbl():
     from dwh_pipelines.L1_raw_layer.raw_flight_schedules_tbl            import  load_flight_schedules_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_schedules_tbl'
     imported_function = 'load_flight_schedules_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -156,9 +105,9 @@ def load_data_to_raw_flight_ticket_sales_tbl():
     from dwh_pipelines.L1_raw_layer.raw_flight_ticket_sales_tbl         import  load_flight_ticket_sales_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_flight_ticket_sales_tbl'
     imported_function = 'load_flight_ticket_sales_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -167,9 +116,9 @@ def load_data_to_raw_sales_agents_tbl():
     from dwh_pipelines.L1_raw_layer.raw_sales_agents_tbl                import  load_sales_agents_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_sales_agents_tbl'
     imported_function = 'load_sales_agents_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -178,9 +127,9 @@ def load_data_to_raw_ticket_prices_tbl():
     from dwh_pipelines.L1_raw_layer.raw_ticket_prices_tbl               import  load_ticket_prices_data_to_raw_table
     module_name = 'dwh_pipelines.L1_raw_layer.raw_ticket_prices_tbl'
     imported_function = 'load_ticket_prices_data_to_raw_table'
-    root_logger.info("")
-    root_logger.info(f"Now importing '{imported_function}' function from '{module_name}' module...")
-    root_logger.info("")
+    get_run_logger().info("")
+    get_run_logger().info(f"Now importing '{imported_function}' function from '{module_name}' module...")
+    get_run_logger().info("")
 
 
 
@@ -198,57 +147,57 @@ def generate_source_data_flow():
 def run_raw_layer_flow():
 
     load_data_to_raw_accommodation_bookings_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_accommodation_bookings_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_accommodation_bookings_tbl'! ")
 
 
 
     load_data_to_raw_customer_feedbacks_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_customer_feedbacks_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_customer_feedbacks_tbl'! ")
 
 
 
     load_data_to_raw_customer_info_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_customer_info_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_customer_info_tbl'! ")
 
 
 
     load_data_to_raw_flight_bookings_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_bookings_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_flight_bookings_tbl'! ")
 
 
 
     load_data_to_raw_flight_destinations_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_destinations_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_flight_destinations_tbl'! ")
 
 
 
     load_data_to_raw_flight_promotion_deals_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_promotion_deals_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_flight_promotion_deals_tbl'! ")
 
 
 
     load_data_to_raw_flight_schedules_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_schedules_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_flight_schedules_tbl'! ")
 
 
 
     load_data_to_raw_flight_ticket_sales_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_flight_ticket_sales_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_flight_ticket_sales_tbl'! ")
 
 
 
     load_data_to_raw_sales_agents_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_sales_agents_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_sales_agents_tbl'! ")
 
 
 
     load_data_to_raw_ticket_prices_tbl()
-    root_logger.info("SUCCESS! Completed loading source data into 'raw_ticket_prices_tbl'! ")
+    get_run_logger().info("SUCCESS! Completed loading source data into 'raw_ticket_prices_tbl'! ")
 
 
-    root_logger.info("Now terminating session for raw layer tasks...")
-    root_logger.info("")
-    root_logger.info("Raw tables processing session ended.")
+    get_run_logger().info("Now terminating session for raw layer tasks...")
+    get_run_logger().info("")
+    get_run_logger().info("Raw tables processing session ended.")
 
 
 
