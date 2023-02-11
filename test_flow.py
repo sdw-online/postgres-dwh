a04801d0f32e0cd5b7b3774a931450e160c60e55
SDW <stephenoilesanmi@gmail.com>
2023-02-11 05:53:03 +0000

    added prefect DAG for raw layer

--- test_flow.py
+++ test_flow.py
@@ -1,15 +1,27 @@
-from prefect import flow, Task
+import os 
+import time
+import subprocess
+from prefect import task, Flow, Parameter
 
 
-@flow(log_prints=True)
-def send_a_greeting():
-    print("Good evening Stephen 🤗")
-    print("This is another message to confirm this function works! 🤗")
+@task
+def run_data_generator_script():
+    subprocess.run(["python", "dwh_pipelines/0_src_data_generator/src_data_generator.py"])
+
+@task
+def run_raw_layer_scripts():
+    for filename in os.listdir("dwh_pipelines/1_raw_layer"):
+        if filename.endswith(".py"):
+            subprocess.run(["python", f"dwh_pipelines/1_raw_layer/{filename}"])
 
 
-if __name__ == "__main__":
-    send_a_greeting()
+interval = Parameter("interval", default=600)
+
+with Flow("Execute raw layer scripts", schedule=interval) as flow:
+    generate_data_task = run_data_generator_script()
+    execute_raw_layer_tasks = run_raw_layer_scripts()
 
 
+    generate_data_task.set_upstream(execute_raw_layer_tasks)
 
-    """Formatting Inspiration - https://github.com/PrefectHQ/prefect/blob/main/src/prefect/logging  """
+flow.run()
