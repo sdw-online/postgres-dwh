b4bfed048ee337dfb67c8bbce0455384f52eaf7b
SDW <stephenoilesanmi@gmail.com>
2023-02-19 16:24:19 +0000

    refined test_dim_accommodation_bookings_tbl script for DQ tests

--- dwh_pipelines/L3_semantic_layer/tests/test_dim_accommodation_bookings_tbl.py
+++ dwh_pipelines/L3_semantic_layer/tests/test_dim_accommodation_bookings_tbl.py
@@ -80,6 +80,11 @@ database_name                   =    database
 
 
 
+# ====================================== DATA QUALITY CHECKS ======================================
+# ================================================================================================= 
+
+
+
 # ====================================== TEST 1: DATABASE CONNECTION CHECK ======================================
 
 
@@ -442,6 +447,86 @@ def test_duplicate_records_count():
 
     # Assert the number of uniqueness constraints for the table specified is at least 1
     assert total_no_of_duplicates == 0, f"Duplicate entries detected - {table_name} should contain no duplicate entries."
+
+
+
+
+
+# ====================================== BUSINESS RULES ======================================
+# ============================================================================================= 
+
+
+# ====================================== TEST 15: DATE RANGE CHECK ======================================
+
+
+""" Test the date value in each date column are within the expected date ranges """
+
+def test_date_range_constraints():
+    earliest_date       =       datetime(2012, 1, 1).date()
+    latest_date         =       datetime(2022, 12, 31).date()
+
+    sql_query_1 = f"""                 SELECT      column_name, 
+                                                    data_type 
+                                        FROM        information_schema.columns 
+                                        WHERE       table_name = '{table_name}'  
+                                        ;
+    """
+    cursor.execute(sql_query_1)
+
+    sql_results = cursor.fetchall()
+
+    for sql_result in sql_results:
+        column_name = sql_result[0]
+        actual_data_type = sql_result[1]
+        if actual_data_type == 'date':
+            sql_query_2 = f"""         SELECT {column_name} FROM {schema_name}.{table_name};
+            """
+            cursor.execute(sql_query_2)
+
+            dates = cursor.fetchall()
+
+            # Assert the selected date value in this column is between the earliest and latest date specified   
+            for date in dates:
+                date_value = date[0]
+
+                assert earliest_date <= date_value <= latest_date, f" Date columns should only contain dates between {earliest_date} and {latest_date}. "
+
+
+
+# ====================================== TEST 16: DATE RANGE CHECKS ======================================
+
+
+""" Test the date value in each date column are within the expected date ranges """
+
+def test_date_range_constraints():
+    earliest_date       =       datetime(2012, 1, 1).date()
+    latest_date         =       datetime(2022, 12, 31).date()
+
+    sql_query_1 = f"""                 SELECT      column_name, 
+                                                    data_type 
+                                        FROM        information_schema.columns 
+                                        WHERE       table_name = '{table_name}'  
+                                        ;
+    """
+    cursor.execute(sql_query_1)
+
+    sql_results = cursor.fetchall()
+
+    for sql_result in sql_results:
+        column_name = sql_result[0]
+        actual_data_type = sql_result[1]
+        if actual_data_type == 'date':
+            sql_query_2 = f"""         SELECT {column_name} FROM {schema_name}.{table_name};
+            """
+            cursor.execute(sql_query_2)
+
+            dates = cursor.fetchall()
+
+            # Assert the selected date value in this column is between the earliest and latest date specified   
+            for date in dates:
+                date_value = date[0]
+
+                assert earliest_date <= date_value <= latest_date, f" Date columns should only contain dates between {earliest_date} and {latest_date}. "
 
 
 
