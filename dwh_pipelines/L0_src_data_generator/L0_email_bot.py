82f705652ad0cdda3e1cbb4bd5ccef080e661492
SDW <stephenoilesanmi@gmail.com>
2023-02-26 20:09:12 +0000

    created function for generating log file names

--- dwh_pipelines/L0_src_data_generator/L0_email_bot.py
+++ dwh_pipelines/L0_src_data_generator/L0_email_bot.py
@@ -42,11 +42,33 @@ print(f'Recipient: {message["To"]} ')
 print(f'Subject: {message["Subject"]}')
 
 
-log_directory = os.getenv("L1_LOG_DIRECTORY")
+L0_LOG_DIRECTORY = os.getenv("L0_LOG_DIRECTORY")
+L1_LOG_DIRECTORY = os.getenv("L1_LOG_DIRECTORY")
+L2_LOG_DIRECTORY = os.getenv("L2_LOG_DIRECTORY")
+L3_LOG_DIRECTORY = os.getenv("L3_LOG_DIRECTORY")
+L4_LOG_DIRECTORY = os.getenv("L4_LOG_DIRECTORY")
 
-for log_filename in os.listdir(log_directory):
-    if os.path.isfile(os.path.join(log_directory, log_filename)):
-        print(f'Log filepath: {os.path.join(log_directory, log_filename)} ')
+
+# for log_filename in os.listdir(log_directory):
+#     if os.path.isfile(os.path.join(log_directory, log_filename)):
+#         print(f'Log filepath: {os.path.join(log_directory, log_filename)} ')
+
+
+def get_log_filepaths(log_directory):
+    log_filepaths = []
+    for root, directories, log_files in os.walk(log_directory):
+        for log_filename in log_files:
+            log_filepath = os.path.join(root, log_filename)
+            log_filepaths.append(log_filepath)
+    return log_filepaths
+
+
+
+l0_filepath = get_log_filepaths(L2_LOG_DIRECTORY)
+
+
+for file_path in l0_filepath:
+    print(file_path)
 
 
 # with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
